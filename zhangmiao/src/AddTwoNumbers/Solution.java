103cffbb02972226e89bae6a5a5836f110ddd0e0
zhangmiao <zhangmiao.dream@qq.com>
2018-03-21 13:29:53 +0800

    AddTwoNumber问题解决方法提交

--- zhangmiao/src/AddTwoNumbers/Solution.java
+++ zhangmiao/src/AddTwoNumbers/Solution.java
@@ -1,5 +1,23 @@
 package AddTwoNumbers;
 
+
+/**
+ * 
+ * You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
+
+ * You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+ * Example
+
+ * Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+ * Output: 7 -> 0 -> 8
+ * Explanation: 342 + 465 = 807.
+ * 
+ * @author zhangmiao
+ * 
+ * email:1006299425@qq.com
+ *
+ */
 public class Solution {
 	
 	/**
@@ -7,15 +25,42 @@ public class Solution {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		//测试
+		//测试		
+		ListNode l1 = new ListNode(2);
+		ListNode l11 = new ListNode(4);
+		l1.next = l11;
+		ListNode l12 = new ListNode(3);
+		l11.next = l12;
+
+
+		ListNode l2 = new ListNode(5);
+		ListNode l21 = new ListNode(6);
+		l2.next = l21;
+		ListNode l22 = new ListNode(4);
+		l21.next = l22;
+//		ListNode l23 = new ListNode(9);
+//		l22.next = l23;
+		
+		
+		ListNode resultNode = addTwoNumbers(l1,l2);
+		StringBuffer resultString = new StringBuffer(resultNode.val+"->");
+		ListNode nextNode= resultNode.next;
+		while(nextNode != null) {
+			resultString.append(nextNode.val+"->");
+			nextNode = nextNode.next;
 		}
+		System.out.println("result:"+resultString.toString());
+	}
 	
 	
 	public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
 		int val = l1.val + l2.val;
         ListNode resultNode = new ListNode(val%10);
-        ListNode nextNode = new ListNode(val/10);
+        ListNode nextNode = new ListNode(val/10); //下一位数字
         resultNode.next = nextNode;
+        ListNode preNode = resultNode; //最后一个数字之前的数字
+      
+        //将相同长度的数据进行相加
         while(l1.next != null && l2.next != null) {
         	l1 = l1.next;
         	l2 = l2.next;
@@ -23,25 +68,35 @@ public class Solution {
         	nextNode.val = v%10;
         	ListNode node = new ListNode(v/10);
         	nextNode.next = node;
+        	preNode = nextNode;
         	nextNode = node;
         }
         
+        ListNode surplusNode = null;
         if (l1.next != null) {
-        	l1 = l1.next;
-        	int v = l1.val+ nextNode.val;
-        	nextNode.val = v%10;
-        	ListNode node = new ListNode(v/10);
-        	nextNode.next = node;
-        	nextNode = node;
+        	surplusNode = l1;
         }
         
         if (l2.next != null) {
-        	l2 = l2.next;
-        	int v = l2.val+ nextNode.val;
-        	nextNode.val = v%10;
-        	ListNode node = new ListNode(v/10);
-        	nextNode.next = node;
-        	nextNode = node;
+        	surplusNode = l2;
+        }
+        
+        //将l1或者l2剩余的数字添加
+        if (surplusNode != null) {
+        	while (surplusNode.next != null) {
+        		surplusNode = surplusNode.next;
+        		int v = surplusNode.val+ nextNode.val;
+        		nextNode.val = v%10;
+        		ListNode node = new ListNode(v/10);
+        		nextNode.next = node;
+        		preNode = nextNode;
+        		nextNode = node;
+        	}
+        }
+   
+        //如果最后一个数字为0，则删除
+        if (nextNode.val == 0) {
+        	preNode.next = null;
         }
         return resultNode;
     }
